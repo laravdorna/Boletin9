4f41f69c019de9035a0a882bbd5fdf490d73fe27
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-11-13 12:11:39 +0100

    a. terminado,comprobacion numero positivo

---
+++ Boletin9_3/src/boletin9_3/Boletin9_3.java
@@ -0,0 +1,23 @@
+/*
+ * 3- Implementa un programa que calcule o área dun rectángulo cuxa base e altura pides por teclado .
+ * Asegurate que estos valores sexan positivos , para eso valida os datos .
+
+ */
+package boletin9_3;
+
+/**
+ *
+ * @author Lara Vazquez Dorna
+ */
+public class Boletin9_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Triangulo trianguloPrueba = new Triangulo();
+        trianguloPrueba.calcularAreaTriangulo();
+    }
+
+}
